e1f7fe0690c194ec01c3fb5249df4035acc99467
FrozenCoder <prof.space123@gmail.com>
2020-01-09 14:29:38 +0545

    done

---
+++ front/homepage.js
@@ -0,0 +1,52 @@
+let {log} = console;
+let el = (x) => document.querySelectorAll(x);
+let compileBtn = el("#compile-btn")[0],
+codeForm = el("#mainCode")[0],
+inputBtn = el("#send-input")[0],
+codeInput = el("#code-input")[0],
+outputArea = el("#output-area")[0];
+let compiling = false;
+log("Script Started");
+
+let socket = io();
+socket.on("connect", (dat)=> {
+	log("Connected to WebSocket Server");
+});
+socket.on('output', (dat)=> {
+	if(!dat.error) {
+		outputArea.innerText += dat;
+	}else {
+		outputArea.innerText += dat.msg;
+	}
+});
+socket.on("exited", (x)=>{
+	disableInputs();
+	disableRun(false);
+});
+
+socket.on("disconnect",()=> {
+	log("Connection Closed by Server");
+});
+
+
+compileBtn.onclick = function() {
+	disableRun(); compiling=true;
+	outputArea.innerHTML = "";
+	let mainCode = codeForm.value;
+	socket.emit("compile/python", {
+		code: mainCode
+	});
+	disableInputs(false);
+}
+
+inputBtn.onclick = function() {
+	if(!compiling) return;
+	socket.emit("input",codeInput.value);
+}
+
+function disableInputs(bool=true) {
+	[inputBtn,codeInput].forEach((x)=>x.disabled=bool);
+}
+function disableRun(bool=true) {
+	compileBtn.disabled = bool;
+}
---
+++ server.js
@@ -0,0 +1,79 @@
+
+const http = require('http');
+const express = require("express");
+const socket = require("socket.io");
+const spawn = require('child_process').spawn;
+const fs = require("fs");
+const port = process.env.PORT || 8080;
+let {log} = console;
+
+const server = express();//express app
+const main_server = http.createServer(server); //Server
+const io = socket(main_server);//WebSocket Server
+const serverError = "Server Error";
+//Serving the web-pages to client
+server.use(express.static('./front', {
+	index: "homepage.html"
+}));
+
+//When a new client connects
+io.on("connection", (client)=>{
+	log("A new user connected");
+	let client_id = client.id;
+	let compiling = false;
+	let runner;//Running Process
+	client.on("compile/python", (data)=> {
+		try {
+			compiling = true;
+			let file_path = 'run-env/python/'+client_id;
+			let file = fs.writeFileSync(file_path, data.code);
+			runner = spawn("python3", [file_path]);
+			runner.stdout.on('data', (_buffer) => {
+				client.emit("output", _buffer.toString());
+			});
+			runner.stderr.on("data",(_buffer)=> {
+				client.emit("output", _buffer.toString());
+			})
+			runner.on("exit", (code)=> {
+				log(code)
+				client.emit("exited","");
+			});
+			runner.on("error",(c)=> {
+				client.emit("output",c);
+			});
+		}catch(err) {
+			log(err);
+			client.emit("output", {
+				error: true,
+				msg: serverError
+			});
+		}
+	});
+	client.on("input", (data) => {
+		try {
+			if(!compiling) return;
+			client.emit('output', data+"\n");
+			runner.stdin.write(data+"\n");
+		}catch(err) {
+			log('output', {
+				error: true,
+				msg: serverError
+			})
+		}
+	});
+	client.on("error", (err)=> {
+		console.log("Error");
+	})
+}); 
+
+io.on("close", (client)=> {
+	log("A user disconnected");
+});
+io.on('error', (err)=> {
+	log(err);
+})
+
+main_server.listen(port, (err)=> {
+	if(!err) return log("Server listenting to the port ", port);
+	log(err);
+});
